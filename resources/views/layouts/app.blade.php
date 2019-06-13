9f30f2a402fc901ca6237b48bb7833c773d840c7
Carlos Eduardo <carlosassiss99@gmail.com>
2019-06-13 02:22:08 -0300

    Editando App

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,33 +35,33 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    @if (!Auth::guest())               
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+                            <li class="nav-item" >
+                                <a class="navbar-brand" href="#">Campos</a>
+                            </li>
+                
+                            <li class="nav-item" >
+                                <a class="navbar-brand" href="#">Regionais</a>
+                            </li>
+                
+                            <li class="nav-item" >
+                                <a class="navbar-brand" href="#">Membros</a>
+                            </li>
+                
+                            <li class="nav-item" >
+                                <a class="navbar-brand" href="#">Cargos</a>
+                            </li>
+                
+                            <li class="nav-item" >
+                                <a class="navbar-brand" href="#">Usuários</a>
+                            </li>
 
-                        <li class="nav-item" >
-                            <a class="navbar-brand" href="#">Campos</a>
-                        </li>
-            
-                        <li class="nav-item" >
-                            <a class="navbar-brand" href="#">Regionais</a>
-                        </li>
-            
-                        <li class="nav-item" >
-                            <a class="navbar-brand" href="#">Membros</a>
-                        </li>
-            
-                        <li class="nav-item" >
-                            <a class="navbar-brand" href="#">Cargos</a>
-                        </li>
-            
-                        <li class="nav-item" >
-                            <a class="navbar-brand" href="#">Usuários</a>
-                        </li>
-
-                    </ul>
-
+                        </ul>
+                    @endif
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
